0e0454472d2a692edd7bdd8d73682eafa45d552c
dorado89 <a.dorado160@uniandes.edu.co>
2019-11-29 21:26:20 -0500

    Need to test it

--- Settings.py
+++ Settings.py
@@ -1,5 +1,6 @@
 import os
 
+ANDROID_HOME = os.environ.get('ANDROID_HOME')
 AWS_ACCESS_KEY_ID_SQS = os.environ.get('AWS_ACCESS_KEY_ID_SQS')
 AWS_SECRET_ACCESS_KEY_SQS = os.environ.get('AWS_SECRET_ACCESS_KEY_SQS')
 AWS_REGION_SQS = os.environ.get('AWS_REGION_SQS')
